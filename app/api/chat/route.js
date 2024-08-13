e85201b7d7c24b811f589463d60b148d7595197a
Shayan I Janjua <shayanjanjua.london@gmail.com>
2024-08-13 22:23:02 +0100

    safety fix

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,10 +1,31 @@
 import { NextResponse } from 'next/server'; // Correct import
 const { GoogleGenerativeAI } = require('@google/generative-ai');
+import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
+const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_NONE,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+  ];
+
 const model = genAI.getGenerativeModel({
   model: 'gemini-1.5-flash',
+  safetySettings
 });
 
 export async function POST(req) {
